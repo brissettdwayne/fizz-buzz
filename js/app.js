1c9c9dc5d48f200c41b371c1878be1c07cf50d00
brissettdwayne <brissettdwayne@gmail.com>
2015-09-13 15:38:30 -0400

    FInished

--- js/app.js
+++ js/app.js
@@ -4,35 +4,19 @@ $(document).ready(function() {
 		for (var i = 1; i <= someNum; i++) {
 			if (i % 3 == 0 && i % 5 == 0) {
 				console.log("FizzBuzz");
-<<<<<<< HEAD
 				$('p').append("FizzBuzz<br>");
-	}
-			else if (i % 5 == 0) {
-				console.log("Buzz");
-				$('p').append("Buzz<br>");
 	}
 			else if (i % 3 == 0) {
 				console.log("Fizz");
 				$('p').append("Fizz<br>");
 	}
-			else {
-				console.log(i);
-				$('p').append(i+"<br>");
-=======
-				$("p").append("FizzBuzz<br>");
-	}
 			else if (i % 5 == 0) {
 				console.log("Buzz");
 				$("p").append("Buzz<br>");
 	}
-			else if (i % 3 == 0) {
-				console.log("Fizz");
-				$("p").append("Fizz<br>");
-	}
 			else {
 				console.log(i);
-				$("p").append(i+"<br>")
->>>>>>> master
+				$("p").append(i+"<br>");
 	};
 };
 	$('p').append();
