aec5f8f5b9df3fc5fa319f38964c4c0addd5e966
brissettdwayne <brissettdwayne@gmail.com>
2015-09-13 15:14:28 -0400

    Touches

--- js/app.js
+++ js/app.js
@@ -3,15 +3,19 @@ $(document).ready(function() {
 		for (var i = 1; i <= 100; i++) {
 			if (i % 3 == 0 && i % 5 == 0) {
 				console.log("FizzBuzz");
+				$("p").append("FizzBuzz<br>");
 	}
 			else if (i % 5 == 0) {
 				console.log("Buzz");
+				$("p").append("Buzz<br>");
 	}
 			else if (i % 3 == 0) {
 				console.log("Fizz");
+				$("p").append("Fizz<br>");
 	}
 			else {
 				console.log(i);
+				$("p").append(i+"<br>")
 	};
 };
 });
