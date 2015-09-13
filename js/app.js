1b8769d35a70b0d44171972ceda79650fb2a0f1c
brissettdwayne <brissettdwayne@gmail.com>
2015-09-13 14:35:57 -0400

    Finished Assignment

--- js/app.js
+++ js/app.js
@@ -1,18 +1,24 @@
 $(document).ready(function() {
 	$("#add-button").click(function() {
-		for (var i = 1; i <= 100; i++) {
+		var someNum = document.getElementById("text-box").value;
+		for (var i = 1; i <= someNum; i++) {
 			if (i % 3 == 0 && i % 5 == 0) {
 				console.log("FizzBuzz");
+				$('p').append("FizzBuzz<br>");
 	}
 			else if (i % 5 == 0) {
 				console.log("Buzz");
+				$('p').append("Buzz<br>");
 	}
 			else if (i % 3 == 0) {
 				console.log("Fizz");
+				$('p').append("Fizz<br>");
 	}
 			else {
 				console.log(i);
+				$('p').append(i+"<br>");
 	};
 };
+	$('p').append();
 });
 });
